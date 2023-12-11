7a8cde56e3e53bc8d913511dbfe1174320f30a92
t0phil04 <t0phil04@louisville.edu>
2023-12-10 23:08:22 -0500

    Add project files.

---
+++ MainForm.cpp
@@ -0,0 +1,14 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+// Entry point of the application
+void main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	FirstApp::MainForm form;
+	Application::Run(% form);
+
+}
+
